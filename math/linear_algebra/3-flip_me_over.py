1ab1c4be519593471e91472eff9fd654dc98774b
Niyobelyse <ganzabelyse@gmail.com>
2025-05-23 19:21:18 +0200

    matrix transpose

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module that contains a function to transpose a 2D matrix
+"""
 def matrix_transpose(matrix):
     """
     Transpose a 2D matrix
@@ -9,7 +12,11 @@ def matrix_transpose(matrix):
     Returns:
         A new list of lists representing the transposed matrix
     """
-    if not matrix or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not matrix or not isinstance(matrix, list):
+        return []
+    
+    if not all(isinstance(row, list) for row in matrix):
         return []
 
-    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return [[matrix[j][i] for j in range(len(matrix))]
+            for i in range(len(matrix[0]))]
