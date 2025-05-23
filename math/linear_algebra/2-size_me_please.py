5a192fc04c627f3881f01277de1ce6cfd50fc704
Niyobelyse <ganzabelyse@gmail.com>
2025-05-23 19:09:11 +0200

    remove spaces

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """
 Function that calculates the shape of a matrix
@@ -7,22 +8,21 @@ Function that calculates the shape of a matrix
 def matrix_shape(matrix):
     """
     Calculates the shape of a matrix
-    
-    payload:
+
+    Args:
         matrix: A list of lists representing a matrix
-        
-    response:
+
+    Returns:
         A list of integers representing the shape of the matrix
     """
     if not matrix:
         return [0]
-    
+
     shape = []
     current = matrix
-    
 
     while isinstance(current, list) and current:
         shape.append(len(current))
         current = current[0]
-    
+
     return shape
