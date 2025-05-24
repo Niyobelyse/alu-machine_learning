74f32ed67774fdcfd016d9faa1c63a823d8cc876
Niyobelyse <ganzabelyse@gmail.com>
2025-05-24 11:06:21 +0200

    removed spaces

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -7,10 +7,8 @@ Function that transposes a numpy.ndarray
 def np_transpose(matrix):
     """
     Transposes a numpy.ndarray
-
     Args:
         matrix: A numpy.ndarray
-
     Returns:
         A new numpy.ndarray that is the transpose of the input matrix
     """
