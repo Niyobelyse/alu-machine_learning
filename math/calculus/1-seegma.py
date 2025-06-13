94692dd22a5e05d39e989cb58529e82c547fb161
Niyobelyse <ganzabelyse@gmail.com>
2025-06-13 16:05:15 +0200

    documented

--- math/calculus/1-seegma.py
+++ math/calculus/1-seegma.py
@@ -1,2 +1,10 @@
+# All your files should end with a new line
+# The first line of all your files should be exactly #!/usr/bin/env python3
+# A README.md file, at the root of the folder of the project, is mandatory
+# Your code should use the pycodestyle style (version 2.5)
+# All your modules should have documentation
+
 #!/usr/bin/env python3
+"""Calculate the sum of a series of numbers using sigma notation.
+This script demonstrates the use of sigma notation to sum a series of integers."""
 4
